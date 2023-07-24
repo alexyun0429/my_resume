1b21a9554ea2ebb896624fd4f7a4ec0a01379bf5
JaeWon Yun <alexyun0429@gmail.com>
2023-07-24 15:52:33 +1000

    working on resume page

--- my-resume/src/pages/Resume/Resume.js
+++ my-resume/src/pages/Resume/Resume.js
@@ -1,8 +1,30 @@
 import React from 'react'
 
+import './Resume.css'
+
 const Resume = () => {
   return (
-    <div>This is the Resume</div>
+    <>
+      {/* About me */}
+      <Grid container className=''>
+
+      </Grid>
+
+      {/* Education and experience */}
+      <Grid container className=''>
+            
+      </Grid>
+
+      {/* skills */}
+      <Grid container className=''>
+            
+      </Grid>
+
+      {/* Contact */}
+      <Grid container className=''>
+            
+      </Grid>
+    </>
   )
 }
 
