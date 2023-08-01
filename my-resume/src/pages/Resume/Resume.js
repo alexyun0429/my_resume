38fdeae02cbbb51052a81c2ae71146032cab24d4
JaeWon Yun <alexyun0429@gmail.com>
2023-08-01 16:01:13 +1000

    almost finished resume

--- my-resume/src/pages/Resume/Resume.js
+++ my-resume/src/pages/Resume/Resume.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './Resume.css';
 import resumeData from '../../utils/resumeData';
+import CustomTimeline, { CustomTimelineSeparator } from '../../components/Timeline/Timeline';
 
 import { Grid, Typography } from '@mui/material';
+import TimelineItem from "@mui/lab/TimelineItem";
+import TimelineContent from "@mui/lab/TimelineContent";
+import WorkIcon from '@mui/icons-material/Work';
+import SchoolIcon from '@mui/icons-material/School';
 
 const Resume = () => {
+
+  const [isExpanded, setIsExpanded] = useState(
+    new Array(resumeData.experiences.length).fill(false)
+  );
+  
+  const toggleExpanded = (index) => {
+    setIsExpanded((prevIsExpanded) =>
+      prevIsExpanded.map((value, i) => (i === index ? !value : value))
+    );
+  };
+  
   return (
     <>
       {/* About me */}
@@ -15,13 +31,72 @@ const Resume = () => {
           <h6 className='section_title_text'> About me </h6>
         </Grid>
         <Grid item xs={12} >
-          <Typography variant='body2' className='aboutme_text'>{resumeData.about}</Typography>
+          <Typography variant='body2 ' className='aboutme_text'>
+            {resumeData.about}
+          </Typography>
         </Grid>
       </Grid>
 
       {/* Education and experience */}
       <Grid container className='section'>
-            
+        <Grid item className='section_title mb_30'>
+          <span></span>
+          <h6 className='section_title_text'>Resume</h6>
+        </Grid>
+
+        <Grid item xs={12}>
+          <Grid container className='resume_timeline'>
+
+            {/* Experiences */}
+            <Grid item sm={12} md={6}>
+              <CustomTimeline title='Work Experience' icon={<WorkIcon />}>
+                {resumeData.experiences.map((experience, index) => (
+                  <TimelineItem>
+                    <CustomTimelineSeparator />
+                    <TimelineContent className='timeline_content'>
+                      <Typography className='timeline_title'>{experience.title}</Typography>
+                      <Typography variant='caption' className='timeline_date'>
+                        {experience.date}
+                      </Typography>
+                      {isExpanded[index] ? (
+                        <Typography variant='body2' className='timeline_description'>
+                          {experience.description}
+                        </Typography>
+                      ) : (
+                        <div>
+                          <Typography variant='body2' className='timeline_description'>
+                            {experience.description.slice(0, 100) + '...'}
+                          </Typography>
+                          <button className='timeline_description_limitbutton' onClick={() => toggleExpanded(index)}>Show more</button>
+                        </div>
+                      )}
+                    </TimelineContent>
+                  </TimelineItem>
+                ))}
+              </CustomTimeline>;
+            </Grid>
+
+            {/* Education */}
+            <Grid item sm={12} md={6}>
+              <CustomTimeline 
+                  title='Education' 
+                  icon={<SchoolIcon />}>
+                    {resumeData.educations.map((education) => (
+                      <TimelineItem>
+                        <CustomTimelineSeparator />
+                        <TimelineContent className='timeline_content'>
+                          <Typography className='timeline_title'>{education.title}</Typography>
+                          <Typography variant='subtitle2' className='timeline_date'>{education.bachelor}</Typography>
+                          <Typography variant='caption' className='timeline_date'>{education.date}</Typography>
+                          <Typography variant='body2' className='timeline_description'>{education.description}</Typography>
+                        </TimelineContent>
+                      </TimelineItem>
+                    ))}
+              </CustomTimeline>
+            </Grid>
+
+          </Grid>
+        </Grid>
       </Grid>
 
       {/* skills */}
--- my-resume/src/utils/resumeData.js
+++ my-resume/src/utils/resumeData.js
@@ -22,5 +22,34 @@ export default {
             icon: <GitHubIcon /> 
         }
     },
+
     about: "As a final year student pursuing a Bachelor of Information Technology at the University of Queensland, I am a passionate, forward-thinking individual with a genuine love for coding and technology. My expertise lies in JavaScript, Python, Java, MySQL, PHP, HTML, and CSS, and I am proficient in various frameworks such as React.js, React Native, CodeIgniter 4, Flask, and NoSQL. \n \n My journey as a Frontend Developer intern at AFIO Group's AETRIEM Labs has provided me with valuable experience in collaborating effectively with cross-functional teams and stakeholders. I take pride in my ability to communicate complex ideas clearly, ensuring seamless coordination and understanding among team members. Beyond my technical capabilities, I have taken on UI/UX responsibilities, creating intuitive and visually appealing interfaces that prioritise usability and enhance the overall user experience. \n \n With a commitment to continuous learning and exploration of new technologies, I am ready to contribute my skills and passion for development to a growth-focused organisation. My personal project portfolio on GitHub showcases my dedication to staying up-to-date with industry advancements. As a Junior Front End Developer, I am eager to make a meaningful impact and embrace new challenges with strong adaptability and technical expertise.",
+
+    experiences: [
+        {
+            title: 'Frontend Developer Intern',
+            date: 'April 2023 - Present',
+            description: "As a Frontend Developer at AETRIEM Labs, I am continuously expanding my skill set and staying up-to-date with the latest advancements in frontend development. I actively seek opportunities to learn and apply new technologies and techniques to enhance my abilities. \n In addition to my technical expertise, I possess strong communication skills, enabling me to collaborate with cross-functional teams and stakeholders effectively. I take pride in my ability to convey complex ideas clearly and concisely, ensuring seamless coordination and understanding among team members. \n Moreover, I have taken on the responsibility of handling the entire project's essential UI/UX aspects. This includes conceptualising, designing, and implementing user interfaces that prioritise usability and enhance the overall user experience. My attention to detail and understanding of user- centred design principles contribute to creating intuitive and visually appealing interfaces. \n Please note that further details about my projects and accomplishments are restricted due to the Non-Disclosure Agreement in place.",
+        },
+        {
+            title: 'Sergeant at the 55th Division 172 brigade',
+            date: 'Jun 2020 - Dec 2021',
+            description: "Republic of Korea Army - Squad leader and chemical equipment engineering officer. Over six months, I structured, trained, collaborated, and led a squad of eight. In the technical field, involved in maintaining, repairing, and managing the brigade (3000 troops), and documenting the processes. Learn efficient problem-solving skills under high pressure and strong teamwork and leadership.",
+        }
+    ],
+
+    educations: [
+        {
+            title: 'University of Queensland, Australia',
+            date: 'Feb 2019, Feb 2022 - Present',
+            bachelor: 'Bachelor of Information Technology',
+            description: "As a Frontend Developer at AETRIEM Labs, I am continuously expanding my skill set and staying up-to-date with the latest advancements in frontend development. I actively seek opportunities to learn and apply new technologies and techniques to enhance my abilities. \n In addition to my technical expertise, I possess strong communication skills, enabling me to collaborate with cross-functional teams and stakeholders effectively. I take pride in my ability to convey complex ideas clearly and concisely, ensuring seamless coordination and understanding among team members. \n Moreover, I have taken on the responsibility of handling the entire project's essential UI/UX aspects. This includes conceptualising, designing, and implementing user interfaces that prioritise usability and enhance the overall user experience. My attention to detail and understanding of user- centred design principles contribute to creating intuitive and visually appealing interfaces. \n Please note that further details about my projects and accomplishments are restricted due to the Non-Disclosure Agreement in place.",
+        },
+        {
+            title: 'Asia Pacific University, Malaysia',
+            date: 'Sept 2016 - Sept 2018',
+            bachelor: 'Diploma in ICT with a specialism in Software Engineering',
+            description: "Republic of Korea Army - Squad leader and chemical equipment engineering officer. Over six months, I structured, trained, collaborated, and led a squad of eight. In the technical field, involved in maintaining, repairing, and managing the brigade (3000 troops), and documenting the processes. Learn efficient problem-solving skills under high pressure and strong teamwork and leadership.",
+        }
+    ]
 }    
